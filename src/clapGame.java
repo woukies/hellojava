5bf6e931d316f036e137e06023e1c1ea31953d66
GreenArt <noreply@example.com>
2021-07-12 15:12:10 +0900

    clapGame: 3의 배수, 5의 배수

--- src/clapGame.java
+++ src/clapGame.java
@@ -20,16 +20,24 @@ public class clapGame {
 		}
 
 		for (int i = 1; i <= inputNumber; i++) {
+			//// 3의 배수
+			// i % 3 == 0
+
+			//// 3,6,9 포함
 			// Integer.toString(i).contains("3") || Integer.toString(i).contains("6") ||
 			// Integer.toString(i).contains("9")
-			// i % 3 == 0
-			if (Integer.toString(i).matches(".*(3|6|9).*")) {
+
+			// Integer.toString(i).matches(".*(3|6|9).*")
+
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("수박");
+			} else if (i % 3 == 0 || i % 5 == 0) {
 				System.out.println("박수");
 			} else {
 				System.out.println(i);
 			}
 		}
-		
+
 		scanner.close();
 	}
 }
