d023f839c55c04b64ec5aa59b52fd72254a47be0
GreenArt <noreply@example.com>
2021-07-13 18:09:19 +0900

    FileChooserTest(example): more

--- src/example/FileChooserTest.java
+++ src/example/FileChooserTest.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 public class FileChooserTest extends JFrame implements ActionListener {
 	JButton openButton, saveButton;
 	JFileChooser fc;
+	JLabel textLabel, paintLabel;
+	JPanel panelNorth, panelCenter;
 
 	public FileChooserTest() {
 		setTitle("파일 선택기 테스트");
@@ -16,18 +18,23 @@ public class FileChooserTest extends JFrame implements ActionListener {
 
 		fc = new JFileChooser();
 
-		JLabel label = new JLabel("파일 선택기 컴포넌트 테스트입니다.");
+		textLabel = new JLabel("파일 선택기 컴포넌트 테스트입니다.");
 		openButton = new JButton("파일 오픈");
 		openButton.addActionListener(this);
 
 		saveButton = new JButton("파일 저장");
 		saveButton.addActionListener(this);
 
-		JPanel panel = new JPanel();
-		panel.add(label);
-		panel.add(openButton);
-		panel.add(saveButton);
-		add(panel);
+		panelNorth = new JPanel();
+		panelNorth.add(textLabel);
+		panelNorth.add(openButton);
+		panelNorth.add(saveButton);
+		add(panelNorth, BorderLayout.NORTH);
+		panelNorth.setPreferredSize(new Dimension(20, 60));
+
+		paintLabel = new JLabel();
+
+		add(paintLabel, BorderLayout.CENTER);
 		setVisible(true);
 	}
 
@@ -35,10 +42,13 @@ public class FileChooserTest extends JFrame implements ActionListener {
 
 		// “파일 오픈”버튼에 대한 액션 이벤트 처리
 		if (e.getSource() == openButton) {
+			fc.setCurrentDirectory(new File("D:\\springframework\\hello-world\\res"));
 			int returnVal = fc.showOpenDialog(this);
 
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				// 실제 파일을 오픈한다.
+				File selectedFile = fc.getSelectedFile();
+				paintLabel.setIcon(new ImageIcon(selectedFile.getPath()));
 			} else {
 				// 사용자 취소
 			}
@@ -48,6 +58,8 @@ public class FileChooserTest extends JFrame implements ActionListener {
 			int returnVal = fc.showSaveDialog(this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				// 실제 파일에 저장한다.
+				File selectedFile = fc.getSelectedFile();
+				System.out.println(selectedFile.getName());
 			} else {
 				// 사용자 취소
 			}
