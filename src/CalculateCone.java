4281d5da5c6825daf6869b5633d584296c7183e8
GreenAcademy <example@noreply.com>
2021-07-27 13:43:20 +0900

    평가 최종 수정

--- src/CalculateCone.java
+++ src/CalculateCone.java
@@ -1,23 +1,22 @@
-package redo;
-
 import java.util.Scanner;
 
 public class CalculateCone {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
 		double radius, height, volume;
 
 		System.out.print("원뿔 밑의 원의 반지름을 입력하세요: ");
-		radius = scanner.nextInt();
+		radius = scan.nextDouble();
 		System.out.print("원뿔의 높이를 입력하세요: ");
-		height = scanner.nextInt();
+		height = scan.nextDouble();
+		
 		volume = radius * radius * Math.PI * height / 3;
 		System.out.println("원뿔 밑의 원의 반지름: " + radius);
 		System.out.println("원뿔의 높이: " + height);
 		System.out.println("원뿔의 부피: " + volume);
 		
-		scanner.close();
+		scan.close();
 	}
 }
--- src/PasswordCheck.java
+++ src/PasswordCheck.java
@@ -1,28 +1,30 @@
-package redo;
-
 import java.util.Scanner;
 
-public class PasswordWhile {
+public class PasswordCheck {
 
 	public static void main(String[] args) {
-		final int maxTry = 3;
+		final int MAX_TRY = 3;
+		
 		Scanner scanner = new Scanner(System.in);
 
 		String input, password = "myongji60";
 		int trynumber = 0;
 
-		for (int i = 0; i < maxTry; i++) {
-			System.out.println("암호를 입력하세요: ");
+		for (int i = 0; i < MAX_TRY; i++) {
+			System.out.print("암호를 입력하세요: ");
 			input = scanner.next();
 			trynumber++;
 
 			if (password.equals(input)) {
 				System.out.println("환영합니다");
 				break;
+			} else {
+				System.out.println("암호가 틀립니다");
 			}
 		}
-		if(trynumber == 3) {
-			System.out.println("접속을 거부합니다.");
+		
+		if(trynumber == MAX_TRY) {
+			System.out.println("접속을 거부합니다");
 		}
 		
 		scanner.close();
