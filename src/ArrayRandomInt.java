be40c04d56d796ccc28ecd222db8e6b58810f838
Woukies <woukiestar@gmail.com>
2021-08-05 17:50:01 +0900

    반복문 하나로

--- src/ArrayRandomInt.java
+++ src/ArrayRandomInt.java
@@ -2,21 +2,16 @@ public class ArrayRandomInt {
 	public static void main(String[] args) {
 		int[][] array = new int[3][4];
 		int sum = 0;
-
+		
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
 				array[i][j] = (int) (Math.random() * 10);
-			}
-		}
-
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				System.out.print(array[i][j] + "\t");
 				sum += array[i][j];
+				System.out.print(array[i][j] + "\t");
 			}
 			System.out.println();
 		}
-
+		
 		System.out.println("합은 " + sum);
 	}
 }
